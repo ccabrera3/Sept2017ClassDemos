cb6dca09b8d5d8802e77568485b85f5db236590e
ccabrera3 <ccabrera3@studentmail.nait.ca>
2017-11-06 14:26:16 -0700

    chinook playlist

---
+++ ClassDemo/Chinook.Data/POCOs/UserPlaylistTrack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.POCOs
+{
+   public class UserPlaylistTrack
+    {
+        public int TrackID { get; set; }
+        public string Title { get; set; }
+        public double TimeLength { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int TrackNumber { get; set; }
+    }
+}
